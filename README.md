bd35a7fbd559be25648516de52fbadac5d2cb766
tonimatias <tonimatias96@gmail.com>
2021-12-31 11:48:55 +0200

    sivuston viimeistely ja siistiminen

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # FinalAssignmengJS
-UI: header, missä otsikko säästä. keskellä on kenttä mihin kirjottaa kaupunki ja submit nappi sille.
+UI: header, missä otsikko säästä. Keskellä on kenttä mihin kirjottaa kaupunki ja submit nappi sille.
 tulostus tulee alas. Footeriin copyright Toni Leinonen ja openWeatherMap
 
 Fontti: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
 
 Pääväri: #E6BB19
 Toinen väri: #EBC947
+Headerin, submit napin ja footerin tekstin väri: rgb(228, 60, 60)
--- weather.js
+++ weather.js
@@ -1,43 +1,27 @@
-// let xmlhttp = new XMLHttpRequest();
-// xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=cbf857554fc18aecbe1686533f317710",true);
-// //https://api.openweathermap.org/data/2.5/weather?q=oulu&units=metric&appid=cbf857554fc18aecbe1686533f317710
-// // Send request
-// xmlhttp.send();
-// // Let's create an event handler
-// xmlhttp.onreadystatechange=function() {
-//     if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
-//         // Response is ready
-//         // Let's parse JSON response
-//         let answerObject = JSON.parse(xmlhttp.response)
-//         // Let's add textual answer
-//         document.getElementById("temperature").innerHTML = answerObject.main.temp;
-//         document.getElementById("feelslike").innerHTML = answerObject.main.feels_like
-//         // Let's add an image
-//         // document.getElementById("answer-image").setAttribute('src', answerObject.image);
-//     }
-//   }
-
+// Tehdään muuttujat jokaiselle tarvittavalle elementille
 let button = document.querySelector(".button")
 let inputValue = document.querySelector(".inputValue")
 let cityName = document.querySelector(".name");
 let desc = document.querySelector(".desc");
 let temp = document.querySelector(".temp");
 
+// funktion check klikkaukseen
 button.addEventListener('click', function(){
+    // Haetaan API tiedot
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=cbf857554fc18aecbe1686533f317710')
     .then(Response => Response.json())
     .then(data => {
+        // Haetaan tiedot
         let nameValue = data['name'];
-        let tempValue = data['main']['temp'];
+        let tempValue = data['main']['temp'] + " °C";
         let descValue = data['weather'][0]['description'];
 
-        cityName.innerHtML = nameValue;
+        // Tulostetaan arvot 
+        cityName.innerHTML = nameValue;
         temp.innerHTML = tempValue;
         desc.innerHTML = descValue
     })
-
-
-.catch(error => alert("Wrong city name!"))
-
+    // Jos käyttäjä syöttää muuta kuin kaupungin nimen, tulee alert
+    .catch(error => alert("Wrong city name!"))
 })
 
